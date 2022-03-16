e7bd7d7ecec9e5dce8122ea57c20dafb0dd71dc3
loose.li <lisong@is-cool.cn>
2022-03-16 13:58:49 +0800

    头像昵称
    信息持久化

--- pages/user/index.js
+++ pages/user/index.js
@@ -4,9 +4,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    loading: false,
     avatar: '',
-    nickName: '',
+    nickname: '',
     isAuth: false,
+    APP_KEY: 'EA2BD5AA28FF7BD4A39F219DF4650EDE',
+    uid: '',
   },
 
   // 点击事件
@@ -20,27 +23,75 @@ Page({
 
   // 监听
   handleTabBarChange() {
-    console.log(111);
-    // 查看是否授权
-    wx.getSetting({
+    const APP_ID = 'wxab00d9afbda623cf';
+    const secret = '5f1d10c89d82fe00f0f55d4391c5584e';
+    wx.login({
       success: res => {
-        console.log(res);
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-          wx.getUserProfile({
-            desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-            success: res => {
-              console.log(res);
-              // var userInfo = res.userInfo;
-              // this.setData({
-              //   nickName: userInfo.nickName,
-              //   avatar: userInfo.avatarUrl,
-              // });
-            },
-          });
-        }
+        wx.request({
+          method: 'GET',
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: APP_ID,
+            secret,
+            js_code: res.code,
+            grant_type: 'authorization_code',
+          },
+          success: res => {
+            this.setData({
+              uid: res.data.openid,
+            });
+            wx.request({
+              method: 'GET',
+              url: 'http://api.yesapi.cn/api/App/Table/GetOneDataByOneField',
+              data: {
+                model_name: 'userInfo',
+                field_name: 'uid',
+                field_value: res.data.openid,
+                app_key: this.data.APP_KEY,
+              },
+              success: res => {
+                console.log(res);
+                this.setData({
+                  loading: true,
+                });
+                const {data} = res;
+                if (data.data.err_code === 3) {
+                  this.setData({
+                    isAuth: false,
+                  });
+                  return;
+                }
+                this.setData({
+                  avatar: data.data.data.avatar,
+                  nickname: data.data.data.nickname,
+                  isAuth: true,
+                });
+              },
+            });
+          },
+        });
       },
     });
+    // 查看是否授权
+    // wx.getSetting({
+    //   success: res => {
+    //     console.log(res);
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+    //       wx.getUserProfile({
+    //         desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+    //         success: res => {
+    //           console.log(res);
+    //           // var userInfo = res.userInfo;
+    //           // this.setData({
+    //           //   nickname: userInfo.nickName,
+    //           //   avatar: userInfo.avatarUrl,
+    //           // });
+    //         },
+    //       });
+    //     }
+    //   },
+    // });
   },
 
   // 获取
@@ -76,9 +127,23 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: res => {
+        console.log(res);
+        wx.request({
+          method: 'POST',
+          url: 'http://api.yesapi.cn/api/App/Table/Create',
+          data: {
+            model_name: 'userInfo',
+            app_key: this.data.APP_KEY,
+            data: {
+              avatar: res.userInfo.avatarUrl,
+              nickname: res.userInfo.nickName,
+              uid: this.data.uid,
+            },
+          },
+        });
         this.setData({
           avatar: res.userInfo.avatarUrl,
-          nickName: res.userInfo.nickName,
+          nickname: res.userInfo.nickName,
           isAuth: true,
         });
       },
