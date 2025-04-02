d65d4fb911e42a936e2b0f25b2b05d6a2dbe7842
LooseLi <1329307562@qq.com>
2025-04-02 14:44:16 +0800

    feat(gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,42 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const minimist = require('minimist');
+
+// 解析命令行参数
+const knownOptions = {
+  string: 'file',
+  default: { file: null }
+};
+const options = minimist(process.argv.slice(2), knownOptions);
 
 // 选择一个预处理器（取消注释您想使用的）
 
 // SCSS 配置
 const sass = require('gulp-sass')(require('sass'));
+
+// 编译所有SCSS文件
 function compileSass() {
   return gulp
     .src(['**/*.scss', '!node_modules/**'])
     .pipe(sass().on('error', sass.logError))
     .pipe(rename({ extname: '.wxss' }))
     .pipe(gulp.dest(file => file.base));
+}
+
+// 编译指定的SCSS文件
+function compileSingleSass() {
+  if (!options.file) {
+    console.error('请指定要编译的文件，例如：gulp sass-file --file=components/DateTimePicker/index.scss');
+    return Promise.resolve();
+  }
+  
+  console.log(`正在编译文件: ${options.file}`);
+  return gulp
+    .src(options.file)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename({ extname: '.wxss' }))
+    .pipe(gulp.dest(file => file.base))
+    .on('end', () => console.log(`文件 ${options.file} 编译完成`));
 }
 
 // LESS 配置
@@ -32,6 +58,7 @@ function watch() {
 
 // 导出任务
 exports.sass = compileSass;
+exports['sass-file'] = compileSingleSass;
 // exports.less = compileLess;
 exports.watch = watch;
 exports.default = watch;
