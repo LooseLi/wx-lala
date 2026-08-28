803921c41f70830f30d31465be844b6aafbf2f27
LooseLi <1329307562@qq.com>
2026-08-28 11:37:30 +0800

    feat(纪念日): 新增截止日期

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,7 +20,8 @@ Page({
     hasAuth: false, //是否有位置权限
     todayUncompletedCount: 0, // 今日未完成待办数量
     themeBackground: '', // 主题背景图片
-    unknow: 'https://6c61-lala-tsum-6gem2abq66c46985-1308328307.tcb.qcloud.la/iconWeathers/wushuju.png?sign=343126b7a94dec3f6074005460ae9d5d&t=1735278996', // 天气图标无数据
+    unknow:
+      'https://6c61-lala-tsum-6gem2abq66c46985-1308328307.tcb.qcloud.la/iconWeathers/wushuju.png?sign=343126b7a94dec3f6074005460ae9d5d&t=1735278996', // 天气图标无数据
     weathers: [],
     announcements: [],
     anniversaryList: [],
@@ -28,7 +29,8 @@ Page({
     holidays: [],
     nowHoliday: null, //当前所处节日
     nextHoliday: null, //下一个节日
-    events: [{
+    events: [
+      {
         id: 0,
         icon: './images/event/icon-jumao.png',
         title: '拉拉松松纪念日',
@@ -177,7 +179,8 @@ Page({
           lives.icon = this.data.unknow;
           lives.tips = '快截图！让松松去更新天气小图标吧~';
         }
-        this.setData({
+        this.setData(
+          {
             today: lives,
           },
           () => {
@@ -336,12 +339,8 @@ Page({
         name: 'getAnniversary',
       });
 
-      const {
-        result
-      } = res;
-      const {
-        data
-      } = result;
+      const { result } = res;
+      const { data } = result;
 
       if (data && data.length > 0) {
         // 处理纪念日数据
@@ -484,9 +483,10 @@ Page({
     wx.loadFontFace({
       global: true,
       family: 'ResourceHanRoundedCN-Medium',
-      source: 'url("https://6c61-lala-tsum-6gem2abq66c46985-1308328307.tcb.qcloud.la/fonts/home/Resource-Han-Rounded-CN-Medium.ttf?sign=929efeb3f1fb0ac8b1d141eec30468bf&t=1781686259")',
+      source:
+        'url("https://6c61-lala-tsum-6gem2abq66c46985-1308328307.tcb.qcloud.la/fonts/home/Resource-Han-Rounded-CN-Medium.ttf?sign=929efeb3f1fb0ac8b1d141eec30468bf&t=1781686259")',
       success: () => console.log('字体加载成功'),
-      fail: (err) => console.error('字体加载失败:', err),
+      fail: err => console.error('字体加载失败:', err),
     });
 
     // 检查本地存储中的食物数据
@@ -553,7 +553,7 @@ Page({
           year,
           month,
           day,
-          dateStr
+          dateStr,
         },
       },
       success: res => {
